0f63b1bc9c3c9c96aad759890e384a3a142a643b
Gabriel D'Nillo <gabrieldnillo99@gmail.com>
2021-06-12 10:31:45 -0300

    question 1018-cedulas refactored with fewer variables

--- beginners/uri/1-problemset-estruturas-basicas/1018-cedulas/cedulas.cpp
+++ beginners/uri/1-problemset-estruturas-basicas/1018-cedulas/cedulas.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int N, i, cen, res, cinq, rescin, vin, resvin, dez, resdez, cinc, rescinc, dois, resdo, um;
+    int N, i, cen, res, cinq, vin, dez, cinc, dois, um;
     char notas[7][7] = {"100,00", "50,00", "20,00", "10,00", "5,00", "2,00", "1,00"};
 
     cin >> N;
@@ -10,16 +10,16 @@ int main() {
     cen = N / 100;
     res = N % 100;
     cinq = res / 50;
-    rescin = res % 50;
-    vin = rescin / 20;
-    resvin = rescin % 20;
-    dez = resvin / 10;
-    resdez = resvin % 10;
-    cinc = resdez / 5;
-    rescinc = resdez % 5;
-    dois = rescinc / 2;
-    resdo = rescinc % 2;
-    um = resdo / 1;
+    res = res % 50;
+    vin = res / 20;
+    res = res % 20;
+    dez = res / 10;
+    res = res % 10;
+    cinc = res / 5;
+    res = res % 5;
+    dois = res / 2;
+    res = res % 2;
+    um = res / 1;
 
     int notasi[7] = {cen, cinq, vin, dez, cinc, dois, um};
 
